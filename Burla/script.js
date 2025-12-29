78467118732855dce43885cc27ea2427d8b9d5c9
Imoya-21 <alguiendelmundo3721@gmail.com>
2025-12-29 01:31:53 +0100

    Update script.js

--- Burla/script.js
+++ Burla/script.js
@@ -67,9 +67,10 @@ finalTrigger.addEventListener('click', () => {
     // 2. Esperar a que termine la animación (1.5s) y redirigir
     setTimeout(() => {
         // Asegúrate de que el nombre del archivo sea exacto
-        window.location.href = "https://imoya-21.github.io/Regalo-navidad/Regalo/Regalo.html"; 
+        window.location.href = "/Regalo-navidad/Regalo/I love you babe.html"; 
     }, 1500);
 
 });
+
 
 
